68e1435ccf53cb1a5bf997f5508dced692094595
ccan <cagdascan@gmail.com>
2013-04-27 14:17:43 +0300

    look at previous comment

--- movieguru.js
+++ movieguru.js
@@ -1,21 +1,55 @@
 Movies = new Meteor.Collection("movies");
+Genres = new Meteor.Collection("genres");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to movieguru.";
-  };
 
-  Template.hello.events({
+  Template.main.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
   });
+
+	Template.genre.element= function () {
+		return Genres.find({}).fetch();
+		
+	};
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+		if (Genres.find().count() == 0) {
+			var genres = 
+			[
+				"Drama",
+				"History",
+				"Romance",
+				"Horror",
+				"Comedy",
+				"Family",
+				"Thriller",
+				"Fantasy",
+				"Adventure",
+				"Action",
+				"Sci-Fi",
+				"Crime",
+				"Biography",
+				"Documentary",
+				"War",
+				"Musical",
+				"Music",
+				"Film-Noir",
+				"Sport",
+				"Mystery",
+				"Animation",
+				"Western",
+				"Short",
+				"Adult"
+			];
+			for (var i=0; i<genres.length; i++)
+				Genres.insert({genre: genres[i]});
+		}
   });
 }

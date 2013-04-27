281a8f3b20732c3c1fe08008f25995184ac3cafe
ccan <cagdascan@gmail.com>
2013-04-27 03:39:19 +0300

    first commit

---
+++ movieguru.js
@@ -0,0 +1,21 @@
+Movies = new Meteor.Collection("movies");
+
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to movieguru.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
